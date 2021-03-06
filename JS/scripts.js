324839c7543a47012e77d4f9fa865d2999efea46
fAlfaro21 <falfarogr@hotmail.com>
2021-03-06 14:34:39 +0100

    Dictionary includes Local Storage Implementation

--- JS/scripts.js
+++ JS/scripts.js
@@ -6,7 +6,13 @@ let result2;
 let index2;
 let click = false;
 let seeMoreDivContainer = [];
-let wordArray = [];
+let paragraphArray = [];
+let detailParagraph = [];
+let detailParagraphId = "";
+let goBackButton = "";
+let userRequestValue = "";
+let requestDiv = "";
+let getMainSearch;
 
 function eraseSeeMoreContainer (seeMoreDivContainer){
   for (let i = 0; i < seeMoreDivContainer.length; i++) {
@@ -14,10 +20,17 @@ function eraseSeeMoreContainer (seeMoreDivContainer){
   };
 }
 
+function eraseContainer(){
+  if (document.querySelector("#results")){
+  document.querySelector("#results").remove();  
+  }
+}
+
 function resetPage(){
   document.getElementById("userRequest")
-          .value = "";
-  window.location.reload();
+        .value = "";
+  eraseContainer();
+  getMainSearch = [];
 }
 
 function createDiv() {
@@ -32,9 +45,9 @@ function createDiv() {
 
 }
 
-function createDivForSeeMore(index2) {
-  
-  seeMoreDiv = document.getElementById(`paragraph${index2}`);
+function createDivForSeeMore(index2) {  
+  //seeMoreDiv = document.getElementById(`paragraph${index2}`);
+  seeMoreDiv = document.getElementById("searchResult");
   printDiv = document.createElement("div"); 
   seeMoreDiv.appendChild(printDiv);
   
@@ -43,12 +56,24 @@ function createDivForSeeMore(index2) {
   printDiv.setAttributeNode(printId);
 
   seeMoreDivContainer.push(printDiv);
+}
 
+function eraseDetailResults(goBackButton){
+  goBackButton.remove();
 }
 
 function createDetailParagraph(quote, index1, index2){
   
-      result2 = document.querySelector(`#seeMore${index2}`);      
+      result2 = document.querySelector(`#seeMore${index2}`); 
+      detailParagraphId = `seeMore${index2}`;
+      
+      printDetailParagraph = document.createElement("h2");
+      printDetailParagraph.innerText = userRequestValue;
+      result2.appendChild(printDetailParagraph);
+
+      printParagraph = document.createElement("h3"); 
+      printParagraph.innerText = paragraphArray[index2];
+      result2.appendChild(printParagraph);      
       
       exampleExists = quote.meanings[index1].definitions[index2].example;
       
@@ -62,31 +87,57 @@ function createDetailParagraph(quote, index1, index2){
         printParagraph.innerText = "No hay ejemplos a mostrar";
         result2.appendChild(printParagraph);
       };
-  
+      return detailParagraphId;  
 }
 
-function retreiveDetailInfo(character, index1, index2){
+function createGoBackButton(index2, stringSearched){
+      goBackButton = document.querySelector(`#seeMore${index2}`);
+      printButton = document.createElement("button"); 
+      printButton.innerText = "Volver";   
+      goBackButton.appendChild(printButton);
 
-  fetch(`https://api.dictionaryapi.dev/api/v2/entries/es/${character}`)
+      printId = document.createAttribute("id");  
+      printId.value = `seeMoreInput${index2}`; 
+      printButton.setAttributeNode(printId);
+
+      goBackButton.addEventListener("click", function() {      
+      eraseDetailResults(goBackButton);
+      //Vuelve a poner lo que había
+      userRequestDiv = document.getElementById("mainDiv");
+      userRequestDiv.appendChild(requestDiv);
+
+      getMainSearch = JSON.parse(window.localStorage.getItem(stringSearched));
+
+      getMainSearch.map(quote =>{
+        createDiv();
+        createParagraph(quote);
+        }); 
+      });
+}
+
+function retreiveDetailInfo(stringSearched, index1, index2){
+  fetch(`https://api.dictionaryapi.dev/api/v2/entries/es/${stringSearched}`)
   .then(response => response.json())  
   .then(data => {
-      data.map(quote =>{
-      
+      data.map(quote =>{      
       createDivForSeeMore(index2);
       createDetailParagraph(quote, index1, index2);
+      createGoBackButton(index2, stringSearched);
       });
   })
   .catch(() => {
       console.log("Error")
       printMessage();
     });
+}
 
-
+function eraseRequestDiv(){
+    requestDiv = document.getElementById("requestDiv");
+    document.getElementById("requestDiv").remove();
 }
 
 function createParagraph(quote){
-  
-  //for (let index1 = 0; index1 < quote.meanings.length; index1++) {
+    //console.log(quote);
     let index1 = 0;
     
     for (let index2 = 0; index2 < quote.meanings[0].definitions.length; index2++) {
@@ -101,13 +152,17 @@ function createParagraph(quote){
       printId.value = `paragraph${index2}`;  
       printParagraph.setAttributeNode(printId);
 
+      paragraphArray.push(`${quote.meanings[0].definitions[index2].definition}`);
+
       document.getElementById(`paragraph${index2}`).addEventListener("click", function() {
-          eraseSeeMoreContainer(seeMoreDivContainer);
-          retreiveDetailInfo(userRequest.value, index1, index2);
+          //userRequestValue =  userRequest.value;
+          eraseRequestDiv();
+          eraseContainer();
+          //eraseSeeMoreContainer(seeMoreDivContainer);//No lo utilizo
+          retreiveDetailInfo(userRequestValue, index1, index2);          
       });
       
     }
-  //}
 }
 
 function printMessage(){
@@ -116,20 +171,22 @@ function printMessage(){
           .innerText = "No se han encontrado resultados para la palabra solicitada";
 }
 
-function dictionaryApi(character) {
-    if(character !== "") {
-      fetch(`https://api.dictionaryapi.dev/api/v2/entries/es/${character}`)
-        .then(response => response.json())  //response pasa el valor al parametro data
+function dictionaryApi(stringSearched) {
+    if(stringSearched !== "") {
+      fetch(`https://api.dictionaryapi.dev/api/v2/entries/es/${stringSearched}`)
+        .then(response => response.json())
         .then(data => {
-            //console.log(data);
-            data.map(quote =>{  //estoy pasando cada elemento de data al parámetro quote
+            data.map(quote =>{  
             createDiv();
             createParagraph(quote);
-            });
+            });            
+            window.localStorage.setItem(stringSearched, JSON.stringify(data));
+            console.log("He buscado en el API la palabra: " + stringSearched);
         })
         .catch(() => {
             console.log("Error")
             printMessage();
+            //.catch(err => console.error(err));
           });
     }
   }
@@ -142,8 +199,23 @@ function dictionaryApi(character) {
   }
 
   document.querySelector('#searchButton').addEventListener("click", function() {
+    paragraphArray = [];
     eraseResults();
-    dictionaryApi(userRequest.value);
+    userRequestValue =  userRequest.value;
+
+    //Si la palabra buscada ya existe en el localStorage, no hace el fecth
+    if(JSON.parse(window.localStorage.getItem(userRequestValue))){
+        createDiv();
+        JSON.parse(window.localStorage.getItem(userRequestValue)).map(quote =>{
+            createDiv();
+            createParagraph(quote);
+            console.log("No he buscado en api: " + userRequestValue);
+        });
+    }
+    //de lo contrario hace el fetch
+    else{
+      dictionaryApi(userRequest.value);
+    }
   });
 
   resetButton.addEventListener ("click", resetPage);
